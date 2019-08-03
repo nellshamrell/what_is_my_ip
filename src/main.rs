2cbf216433fc191780e37d7013cd162f2b98d4ff
Nell Shamrell <nellshamrell@gmail.com>
2019-08-02 17:19:45 -0700

    more methods of looking up ip address
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use dns_lookup::{lookup_host, get_hostname};
 extern crate pnet;
 use pnet::datalink;
 
+use std::net::{IpAddr,
+               UdpSocket};
+
+
 fn main() {
     dns_lookup();
     pnet_lookup();
+    udp_method_lookup();
 }
 
 fn dns_lookup() {
@@ -24,4 +29,18 @@ fn pnet_lookup() {
     for iface in datalink::interfaces() {
         println!("{:?}", iface.ips);
     }
+}
+
+static GOOGLE_DNS: &'static str = "8.8.8.8:53";
+
+// Based on https://github.com/habitat-sh/habitat/blob/4527c32e25d60d77ece99cb7ac8cd1182cb85279/components/core/src/util/sys.rs#L9-L16
+fn udp_method_lookup() {
+    println!("udp_method_lookup");
+
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    socket.connect(GOOGLE_DNS);
+
+    let addr = socket.local_addr().unwrap();
+
+    let ip = addr.ip();
 }
